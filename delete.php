2ce7029101582ac3b4adbe3880ba7b309a6d6d7e
Nicolas Endress <nicolasendress@gmail.com>
2023-10-28 21:23:06 -0300

    Send 403 si no te pertenece

--- delete.php
+++ delete.php
@@ -4,19 +4,27 @@ require "database.php";
 
 session_start();
 
-if (!isset($_SESSION["user"])){
+if (!isset($_SESSION["user"])) {
   header("Location: login.php");
   return;
 }
 
 $id = $_GET["id"];
 
-$statement = $conn->prepare("SELECT * FROM contacts WHERE id = :id");
+$statement = $conn->prepare("SELECT * FROM contacts WHERE id = :id LIMIT 1");
 $statement->execute([":id" => $id]);
 
 if ($statement->rowCount() == 0) {
   http_response_code(404);
-  echo("HTTP 404 NOT FOUND");
+  echo ("HTTP 404 NOT FOUND");
+  return;
+}
+
+$contact = $statement->fetch(PDO::FETCH_ASSOC);
+
+if ($contact["user_id"] !== $_SESSION["user"]["id"]) {
+  http_response_code(403);
+  echo ("HTTP 403 UNAUTHORIZED");
   return;
 }
 
--- edit.php
+++ edit.php
@@ -1,48 +1,54 @@
 <?php
 
-  require "database.php";
+require "database.php";
 
-  session_start();
+session_start();
 
-  if (!isset($_SESSION["user"])){
-    header("Location: login.php");
-    return;
+if (!isset($_SESSION["user"])) {
+  header("Location: login.php");
+  return;
+}
+
+$id = $_GET["id"];
+
+$statement = $conn->prepare("SELECT * FROM contacts WHERE id = :id LIMIT 1");
+$statement->execute([":id" => $id]);
+
+if ($statement->rowCount() == 0) {
+  http_response_code(404);
+  echo ("HTTP 404 NOT FOUND");
+  return;
+}
+
+$contact = $statement->fetch(PDO::FETCH_ASSOC);
+
+if ($contact["user_id"] !== $_SESSION["user"]["id"]) {
+  http_response_code(403);
+  echo ("HTTP 403 UNAUTHORIZED");
+  return;
+}
+
+$error = null;
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  if (empty($_POST["name"]) || empty($_POST["phone_number"])) {
+    $error = "Please fill all the fields.";
+  } else if (strlen($_POST["phone_number"]) < 9) {
+    $error = "Phone number must be at least 9 characters.";
+  } else {
+    $name = $_POST["name"];
+    $phoneNumber = $_POST["phone_number"];
+
+    $statement = $conn->prepare("UPDATE contacts SET name = :name, phone_number = :phone_number WHERE id = :id");
+    $statement->execute([
+      ":id" => $id,
+      ":name" => $_POST["name"],
+      ":phone_number" => $_POST["phone_number"],
+    ]);
+
+    header("Location: home.php");
   }
-
-  $id = $_GET["id"];
-
-  $statement = $conn->prepare("SELECT * FROM contacts WHERE id = :id LIMIT 1");
-  $statement->execute([":id" => $id]);
-
-  if ($statement->rowCount() == 0) {
-    http_response_code(404);
-    echo("HTTP 404 NOT FOUND");
-    return;
-  }
-
-  $contact = $statement->fetch(PDO::FETCH_ASSOC);
-
-  $error = null;
-
-  if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    if (empty($_POST["name"]) || empty($_POST["phone_number"])) {
-      $error = "Please fill all the fields.";
-    } else if (strlen($_POST["phone_number"]) < 9) {
-      $error = "Phone number must be at least 9 characters.";
-    } else {
-      $name = $_POST["name"];
-      $phoneNumber = $_POST["phone_number"];
-
-      $statement = $conn->prepare("UPDATE contacts SET name = :name, phone_number = :phone_number WHERE id = :id");
-      $statement->execute([
-        ":id" => $id,
-        ":name" => $_POST["name"],
-        ":phone_number" => $_POST["phone_number"],
-      ]);
-
-      header("Location: home.php");
-    }
-  }
+}
 ?>
 
 <?php require "parcial/header.php" ?>
@@ -53,7 +59,7 @@
       <div class="card">
         <div class="card-header">Add New Contact</div>
         <div class="card-body">
-          <?php if ($error): ?>
+          <?php if ($error) : ?>
             <p class="text-danger">
               <?= $error ?>
             </p>
